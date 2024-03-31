8dd3dd024201fb9e9a76786de5029683ba7190aa
Komal Bharambe <116254499+Komal-Bharambe@users.noreply.github.com>
2024-03-31 23:43:13 +0530

    Add files via upload

---
+++ src/redux/slices/postsSlices.js
@@ -0,0 +1,55 @@
+import {createAsyncThunk, createSlice} from '@reduxjs/toolkit'
+import { axiosClient } from '../../utils/axiosClient';
+import { setLoading } from './appConfigSlice';
+
+export const getUserProfile = createAsyncThunk('user/getUserProfile', async(body) =>{
+    try{
+      
+        const response = await axiosClient.post('/user/getUserProfile', body);
+        console.log('userprofile response', response);
+        return response.result;
+    }
+    catch(error){
+        return Promise.reject(error);
+    }
+   
+})
+
+export const likeAndUnlikePost = createAsyncThunk('post/likeAndUnlike', async(body) =>
+{
+    try{
+       
+        const response = await axiosClient.post('/posts/like', body);
+        console.log('userprofile response', response);
+        return response.result.post;
+    }
+    catch(error){
+        return Promise.reject(error);
+    }
+  
+})
+
+const postsSlice = createSlice({
+    name: 'postsSlice',
+    initialState:{
+        userProfile:{},
+    },
+   
+    extraReducers:(builder) =>{
+        builder.addCase(getUserProfile.fulfilled, (state, action) =>{
+            state.userProfile = action.payload; 
+        })
+        .addCase(likeAndUnlikePost.fulfilled, (state, action) =>{
+            const post = action.payload;
+            console.log('liked posts', post)
+            const index = state?.userProfile?.posts?.findIndex(item => item._id == post._id);
+            console.log('postSlice', index);
+            if(index != undefined && index != -1){
+                state.userProfile.posts[index] = post;
+             }
+        });
+    },
+});
+
+export default postsSlice.reducer;
+
